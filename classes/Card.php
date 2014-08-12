3d0302470a6aa2a757babd59122b64af59c65fc7
arall <gerard.arall@gmail.com>
2014-08-12 08:18:08 -0700

    1 doesn't exist!

--- classes/Card.php
+++ classes/Card.php
@@ -18,17 +18,13 @@ class Card
      * @param  boolean $lowerAs  Count A as 1, not 11
      * @return int
      */
-    public function getValue($isDealer = false, $lowerAs = false)
+    public function getValue($lowerAs = false)
     {
         // Monkey?
         if (!is_numeric($this->value)) {
             // As
             if ($this->value == "A") {
-                // Dealer
-                if ($isDealer) {
-                    return 1;
-                }
-                // Player
+                // Lower As
                 if ($lowerAs) {
                     return 1;
                 } else {
